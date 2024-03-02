646f30183c40ee9aa4a400e577ad6ee27934315a
Gabriel Narciso <gabriel.narciso2116@gmail.com>
2024-03-01 22:20:41 -0300

    feat(ap04) : fim da secao 2.5
    -4 pedidos sem utilizar componente especifico

--- componentes-props/src/index.js
+++ componentes-props/src/index.js
@@ -11,24 +11,64 @@ const App = () => {
         </div>
         {/* linha dos pedidos */}
         <div className="row">
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className='bg-secondary'>caixa1</div>
-
+            <div className="col-sm-12 col-lg-6 col-xl-3">
+                <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex"></div>
+                    <div className="d-flex align-items-center">
+                        <i className="fa-regular fa-hard-drive fa-2x fa-beat-fade"></i>
+                    </div>
+                    <div className="flex-grow-1 ms-3 border">
+                        <h4 className="text-center">HD</h4>
+                        <p className="text-center">HD 1Tb</p>
+                    </div>
+                </div>
             </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className='bg-secondary'>caixa2</div>
+            <div className="col-sm-12 col-lg-6 col-xl-3">
+            <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex"></div>
+                    <div className="d-flex align-items-center">
+                        <i className="fa-solid fa-memory fa-2x fa-beat-fade"></i>
+                    </div>
+                    <div className="flex-grow-1 ms-3 border">
+                        <h4 className="text-center">RAM</h4>
+                        <p className="text-center">Memoria 16GB</p>
+                    </div>
+                </div>
             </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className='bg-secondary'>caixa3</div>
+            <div className="col-sm-12 col-lg-6 col-xl-3">
+            <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex"></div>
+                    <div className="d-flex align-items-center">
+                        <i className="fa-brands fa-java fa-2x fa-beat-fade"></i>
+                    </div>
+                    <div className="flex-grow-1 ms-3 border">
+                        <h4 className="text-center">Java</h4>
+                        <p className="text-center">Projeto simples</p>
+                    </div>
+                </div>
             </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className='bg-secondary'>caixa4</div>
+            <div className="col-sm-12 col-lg-6 col-xl-3">
+            <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex"></div>
+                    <div className="d-flex align-items-center">
+                        <i className="fa-solid fa-gamepad fa-2x fa-beat-fade"></i>
+                    </div>
+                    <div className="flex-grow-1 ms-3 border">
+                        <h4 className="text-center">Game</h4>
+                        <p className="text-center">Game App</p>
+                    </div>
+                </div>
             </div>    
-         </div>  coo
+         </div>  
     </div>
 }
 
 ReactDOM.render(
     <App />
+
     ,document.querySelector('#root')
 )
