434ee392644d69ce60f7392f8c6941317da70bb0
Gabriel Narciso <gabriel.narciso2116@gmail.com>
2024-03-22 20:15:15 -0300

    feat(ap05): implementa jsx do componente principal

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -4,83 +4,111 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
-   // window.navigator.geolocation.getCurrentPosition((position) => {
-   // console.log(position)
-//})
 
-constructor(props){
+  constructor(props){
     super(props)
-    this.state ={
-        latitude: null,
-        longitude: null,
-        estacao: null,
-        data: null,
-        icone: null
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null
     }
-}
-icones = {
+  }
+
+  icones = {
     'Verão': 'sun',
     'Inverno': 'snowflake',
-    'Outono': 'canadian-maple-leaf',
-    'Primavera': 'tree',
-}
-obterEstacao = (dataAtual, latitude) =>{
+    'Outono': 'leaf',
+    'Primavera': 'tree'
+
+  }
+
+  obterEstacao = (dataAtual, latitude) => {
     const anoAtual = dataAtual.getFullYear()
+    //21/06
     const d1 = new Date(anoAtual, 5, 21)
+    //24/09
     const d2 = new Date(anoAtual, 8, 24)
+    //22/12
     const d3 = new Date(anoAtual, 11, 22)
+    //21/03
     const d4 = new Date(anoAtual, 2, 21)
-    const sul = latitude < 0 
-    if (dataAtual >= d1 && dataAtual <d2)
-        return sul ? 'Inverno': 'Verão'
-    if (dataAtual >= d2 && dataAtual <d3)
-        return sul ? 'Primavera': 'Outono'
-    if (dataAtual >= d3 || dataAtual <=d4)
-        return sul ? 'Verão': 'Inverno'
-    return sul ? 'Outono': 'Primavera'  
-}
+    // const sul = latitude < 0 ? true : false
+    const sul = latitude < 0
+    if (dataAtual >= d1 && dataAtual < d2)
+      return sul ? 'Inverno' : 'Verão'
+    if (dataAtual >= d2 && dataAtual < d3)
+      return sul ? 'Primavera' : 'Outono'
+    if (dataAtual >= d3 || dataAtual <= d4)
+      return sul ? 'Verão' : 'Inverno'
+    return sul ? 'Outono' : 'Primavera'
+  }
 
-obterLocalizacao = () => {
+  obterLocalizacao = () => {
+    
     window.navigator.geolocation.getCurrentPosition(
-     (posicao) => {
-     const dataAtual = new Date()
-     const estacaoClimatica = this.obterEstacao(dataAtual, posicao.coords.latitude);
-     const nomeIcone = this.icones[estacaoClimatica]
-        
-    this.setState({
-        latitude: posicao.coords.latitude,
-        longitude: posicao.coords.longitude,
-        estacao: estacaoClimatica,
-        data: dataAtual,
-        icone: nomeIcone
-            } )
-        },
+     
+      (posicao) => {
 
-    (erro) => {
-        console.error("Geolocalização não tem suporte neste browser.");
-    })
+        const dataAtual = new Date()    
+        const estacaoClimatica = this.obterEstacao(dataAtual, posicao.coords.latitude)    
+        const nomeIcone = this.icones[estacaoClimatica]    
+        this.setState({
+          latitude: posicao.coords.latitude,
+          longitude: posicao.coords.longitude,
+          estacao: estacaoClimatica,
+          icone: nomeIcone,
+          data: dataAtual
+        })
+      },
+      (erro) => {
+        this.setState({mensagemDeErro: 'Tente novamente mais tarde'})
 
-    //*if (navigator.geolocation) {
-       // navigator.geolocation.getCurrentPosition((position) => {
-          //  const latitude = position.coords.latitude;
-          //  const longitude = position.coords.longitude;
-          //  const dataAtual = new Date();
-           // const estacao = this.obterEstacao(dataAtual, latitude);
-           // const icone = this.icones[estacao];
-           // this.setState({ latitude, longitude, estacao, data: dataAtual, icone });
-       // });
-   // } else {
-   //     console.error("Geolocalização nãotem suporte neste browser.");
-   // } 
+      }
+    )
+  }
+
+  
+  render(){
+    return (
+      <div className='container mt-2'>
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-md-8">
+              <div className="card">
+                <div className="card-body">
+                  <div 
+                    className="d-flex align-items-center border rounded mb-2"
+                    style={{height: '6rem'}}>
+                    <i className={`fa-solid fa-5x fa-${this.state.icone}`}></i>
+                    <p className="w-75 ms-3 text-center fs-1">{this.state.estacao}</p>
+                  </div>
+                  <div>
+                    <p className="text-center">
+                      {
+                      this.state.latitude ?
+                        `Coordenadas: ${this.state.latitude, this.state.longitude}. Data: ${this.state.data}` :
+                        this.state.mensagemDeErro ?
+                        `${this.state.mensagemDeErro}` :
+                        `Clique no botão para saber a sua estação climática`
+                      }
+                    </p>
+                  </div>
+                  <button 
+                    className="btn btn-outline-primary w-100 mt-2"
+                    onClick={() => this.obterLocalizacao()}>
+                    Qual a minha estação?
+                  </button>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-render(){
-    return <div>
-    Estação
-</div>
-    }
-}
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 )
